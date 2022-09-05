a4e1f7996d14d5a6a003a2f5f2be1be7f15c3ca6
antony-with-no-h <94751539+antony-with-no-h@users.noreply.github.com>
2022-09-05 15:49:08 +0100

    fix error: not run in chroot

--- stemcell_builder/stages/git_gitaly/apply.sh
+++ stemcell_builder/stages/git_gitaly/apply.sh
@@ -4,7 +4,7 @@ set -ex
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-pkg_mgr install libpcre2-dev
+pkg_mgr install libpcre2-dev libpcre3-dev libexpat1-dev libz-dev
 
 pushd ${base_dir}/gitaly
 make git
